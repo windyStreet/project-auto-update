f6e0e3e1efd148e3543460fd96b6a3b281096004
windyStreet <yq904276384@foxmail.com>
2016-12-02 00:26:57 +0800

    define new judge

--- func/RungroupFunc.py
+++ func/RungroupFunc.py
@@ -4,31 +4,17 @@ import FormatPrint
 import os
 import sys
 import json
+import JsonFileFunc
 
 class RunGroup(object):
     def __init__(self):
         pass
 
-    #获取运行组信息(哪一组正在运行)
-    def getName(selef,projectName):
-        try:
-            with open(sys.path[0]+os.sep+'runtime'+os.sep+'nginx-group-runstatus.json') as nginx_group_runstatus_file:
-                nginx_group_runstatus_JSON=json.load(nginx_group_runstatus_file)
-        except Exception as e:
-            FormatPrint.printFalat("获取运行组信息(哪一组正在运行),nginx-group-runstatus.json出现异常:"+str(e));
-            exit(-1)
-        try:
-            with open(sys.path[0] + os.sep + 'runtime' + os.sep + 'tomcat-group-runstatus.json') as tomcat_group_runstatus_file:
-                tomcat_group_runstatus_JSON = json.load(tomcat_group_runstatus_file)
-        except Exception as e:
-            FormatPrint.printFalat("获取运行组信息(哪一组正在运行),tomcat-group-runstatus.json出现异常:" + str(e));
-            exit(-1)
-        if nginx_group_runstatus_JSON["projectname"][projectName]["currentrun"] == tomcat_group_runstatus_JSON["projectname"][projectName]["currentrun"]:
-            return nginx_group_runstatus_JSON["projectname"][projectName]["currentrun"]
-        else:
-            FormatPrint.printFalat("无法获取"+projectName+"项目运行组的状态")
-            sys.exit(-1)
-
 # 重启tomcat
 def getRunGroupName(projectName):
-    return RunGroup().getName(projectName)
+    nodeHealStatusFile=node_health_status_file = sys.path[0] + os.sep + 'runtime' + os.sep + str(projectName) +'-node-health-status.json'
+    nodeHealStatus = JsonFileFunc.readFile(nodeHealStatusFile)
+    if nodeHealStatus == None:
+        return "groupmaster"
+    else:
+        return nodeHealStatus["currentrun"]
