66522a9ba304bac09930d1f39aaf00269eb0b957
ddd <ooo>
2016-12-01 14:12:26 +0800

    add some

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='project-auto-update',
+    version='v0.0.5',
+    packages=[''],
+    url='windyStreet',
+    license='windyStreet',
+    author='Administrator',
+    author_email='yq904276384@foxmail.com',
+    description='windyStreet'
+)
