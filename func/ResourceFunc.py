50169706efbb150f98154dc600d44a195fa197a1
ddd <ooo>
2016-12-05 09:31:39 +0800

    add remarks and @file

--- func/ResourceFunc.py
+++ func/ResourceFunc.py
@@ -7,7 +7,22 @@ class ResourceFunc(object):
     def __init__(self):
         pass
 
-#资源替换
+'''
+ 资源替换,通过执行参数
+ par:pu{
+    "updateTime":xxxx
+    "updateVersion":xxx
+    "projectname":{
+        "projectName_xxx":{
+            "willUpdateGroup_xxx":{
+                "tomcatresourceupdatescriptpath":xxx
+            }
+        }
+    }
+ }
+功能实现，实现项目资源的替换（lib，ResoutceLib，staticResource）
+备注：脚本执行成功时返回0，脚本执行错误中断更新
+'''
 def replceResource(pu):
     tomcat_conf = pu.projectJson.tomcatConf
     command = tomcat_conf['projectname'][pu.projectName][pu.willUpdateGroup]['tomcatresourceupdatescriptpath'] + " " + str(pu.updateVersion) + " " + str(pu.updateTime)
