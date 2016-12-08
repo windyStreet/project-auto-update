91964e2b3a1b89bb84e0673d669ab55b3715f89d
ddd <ooo>
2016-12-08 11:57:00 +0800

    fix error

--- func/HealthCheck.py
+++ func/HealthCheck.py
@@ -19,24 +19,23 @@ class HealthCheck(object):
 
     def checkAllTime(self):
         while True:
-            self.check(self.projectName)
+            self.check()
             time.sleep(60)
 
 #一直启动检查服务
 def checkAllTime(projectName):
     hc = HealthCheck()
     hc.projectName = projectName
-    hc.checkAllTime(hc.projectName)
+    hc.checkAllTime()
 
 #检查服务单独执行一次
 def checkOnce(projectName):
     hc = HealthCheck()
     hc.projectName = projectName
-    return hc.check(hc.projectName)
+    return hc.check()
 
 #启动健康检查
 def startHealthCheck(projectName):
-    FormatPrint.printDebug("startHealthCheck")
     path=sys.path[0] + os.sep + 'shell' + os.sep + 'healthCheck.sh '
     cmd = path + " start " + str(projectName)
     if os.system(cmd) == 0:
